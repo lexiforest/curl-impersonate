8b501681cb36db45d42eb5bc99cfaec1413e67b8
cr1symo <187496241+cr1symo@users.noreply.github.com>
2024-11-06 12:36:31 +0200

    debian dockerfile patchine "boringssl-*.patch" error

--- docker/generate.sh
+++ docker/generate.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-cat <<EOF | mustache - Dockerfile.mustache > docker/debian.dockerfile
+cat <<EOF | mustache - Dockerfile.mustache > debian.dockerfile
 ---
 debian: true
 ---
 EOF
 
-cat <<EOF | mustache - Dockerfile.mustache > chrome/alpine.dockerfile
+cat <<EOF | mustache - Dockerfile.mustache > alpine.dockerfile
 ---
 alpine: true
 ---
