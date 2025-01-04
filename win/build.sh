bc493c9819a6bc4a29f4d1754ee7f617e177ca8a
Yifei Kong <kong@yifei.me>
2025-01-04 23:14:42 +0800

    Enable USE_HTTPSRR for winbuild

--- win/build.sh
+++ win/build.sh
@@ -65,6 +65,7 @@ cmake -B build -G "MinGW Makefiles" \
     -DCURL_USE_LIBSSH=OFF \
     -DUSE_ECH=ON \
     -DHAVE_ECH=ON \
+    -DUSE_HTTPSRR \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_STATIC_LIBS=ON \
     -DBUILD_STATIC_CURL=ON \
