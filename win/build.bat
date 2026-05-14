7ac4ac0b2a05413a40a6beedd59d853364906f1a
lexiforest <infinitesheldon@gmail.com>
2026-05-14 16:47:08 +0800

    macos polish

--- win/build.bat
+++ win/build.bat
@@ -4,16 +4,44 @@ setlocal EnableExtensions
 set "PATH=%PATH:LLVM=Dummy%"
 
 if "%~1"=="" (
-  set "VCVARS_BAT=vcvars64"
+  set "ARCH=x64"
 ) else (
-  set "VCVARS_BAT=%~1"
+  set "ARCH=%~1"
 )
 
-if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\%VCVARS_BAT%.bat" (
-  call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\%VCVARS_BAT%.bat"
-) else (
-  call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\%VCVARS_BAT%.bat"
+if /I "%ARCH%"=="x86_64" set "ARCH=x64"
+if /I "%ARCH%"=="i686" set "ARCH=x86"
+
+if /I "%ARCH%"=="x64" goto arch_ok
+if /I "%ARCH%"=="x86" goto arch_ok
+if /I "%ARCH%"=="arm64" goto arch_ok
+
+echo Unsupported MSVC architecture "%ARCH%". Expected x64, x86, or arm64.
+exit /b 1
+
+:arch_ok
+set "VC_TOOLS_COMPONENT=Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
+if /I "%ARCH%"=="arm64" set "VC_TOOLS_COMPONENT=Microsoft.VisualStudio.Component.VC.Tools.ARM64"
+
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%VSWHERE%" (
+  echo Could not find vswhere.exe at "%VSWHERE%".
+  exit /b 1
 )
+
+for /f "usebackq tokens=*" %%i in (`"%VSWHERE%" -latest -products * -requires %VC_TOOLS_COMPONENT% -property installationPath`) do set "VSINSTALLDIR=%%i"
+if "%VSINSTALLDIR%"=="" (
+  echo Could not find a Visual Studio installation with MSVC tools.
+  exit /b 1
+)
+
+set "VCVARSALL=%VSINSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat"
+if not exist "%VCVARSALL%" (
+  echo Could not find vcvarsall.bat at "%VCVARSALL%".
+  exit /b 1
+)
+
+call "%VCVARSALL%" %ARCH%
 if errorlevel 1 exit /b 1
 
 set "BUILD_DIR=%cd%\build"
