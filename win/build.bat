061b74e7d99a606333bf1282c2441f6fc825b8b8
lexiforest <infinitesheldon@gmail.com>
2026-05-15 18:49:02 +0800

    Fix Windows CMake build

--- win/build.bat
+++ win/build.bat
@@ -10,11 +10,10 @@ if "%~1"=="" (
 )
 
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\%VCVARS_BAT%.bat" (
-  call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\%VCVARS_BAT%.bat"
+  call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\%VCVARS_BAT%.bat" || exit /b 1
 ) else (
-  call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\%VCVARS_BAT%.bat"
+  call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\%VCVARS_BAT%.bat" || exit /b 1
 )
-if errorlevel 1 exit /b 1
 
 set "BUILD_DIR=%cd%\build"
 set "PACKAGES_DIR=%cd%\packages"
@@ -30,11 +29,9 @@ cmake -S . -B "%BUILD_DIR%" -GNinja ^
   -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ^
   -DCMAKE_C_COMPILER=clang-cl.exe ^
   -DCMAKE_CXX_COMPILER=clang-cl.exe ^
-  -DCMAKE_LINKER=link.exe
-if errorlevel 1 exit /b 1
+  -DCMAKE_LINKER=link.exe || exit /b 1
 
-cmake --build "%BUILD_DIR%" --config %CONFIGURATION% --target install-all
-if errorlevel 1 exit /b 1
+cmake --build "%BUILD_DIR%" --config %CONFIGURATION% --target install-all || exit /b 1
 
 if not exist "%PACKAGES_DIR%\bin" mkdir "%PACKAGES_DIR%\bin"
-copy /Y ".\win\bin\*.bat" "%PACKAGES_DIR%\bin\" >NUL
+copy /Y ".\win\bin\*.bat" "%PACKAGES_DIR%\bin\" >NUL || exit /b 1
